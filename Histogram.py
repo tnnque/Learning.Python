f01804899ce64a468bc5873efe8d7f3c822bbccc
tnnque <tnnque@gmail.com>
2019-11-04 17:51:30 +0700

    Doing

--- Histogram.py
+++ Histogram.py
@@ -1,16 +1,7 @@
 x = input()
 s = x.split()
 
-def plot_matrix(R, C, matrix):
-    for i in range(R):
-        a = []
-        for j in range(C):
-            a.append(int())
-        matrix.append(a)
-    for i in range(R):
-        for j in range(C):
-            print(matrix[i][j], end=" ")
-        print()
+
 def histogram(s):
     d = dict()
     for c in s:
@@ -18,10 +9,58 @@ def histogram(s):
             d[c] = 1
         else:
             d[c] += 1
-    C = len(d)
-    R = max(d[x])
-    for y in d:
-        print(y,d[y])
-histogram(x)
+    return d
 
-def plot_img():
+
+def plotMatrix(mat):
+    for i in mat:
+        for j in i:
+            if j == 1:
+                print('*', end= '')
+            else:
+                print(' ', end= '')
+        print('')
+
+
+def convertDictToMatrix(d):
+    matrix = []
+    max_value = 0
+    # Tim max
+    for key in d:
+        value = d[key]
+        if value >= max_value:
+            max_value = value
+    # Matran 0 1
+    for i in d:
+        x = []
+        # if d[i] ==:
+        for j in range(0, d[i]):
+            x.append(1)
+
+        for k in range(d[i], max_value):
+            x.append(0)
+        matrix.append(x)
+    # return matrix
+    # Rotate 90
+    matrix2 = []
+    # matrix2 = matrix.copy()
+    for i in matrix:
+        sup = []
+        for j in i:
+            if vi tri cua j >= max_value:
+                sup.append(i[j])
+            max_value = max_value - 1
+        matrix2.append(sup)
+    # for i in range(0, len(matrix)):
+
+
+        # for j in range(0, len(matrix[i])):
+
+            # matrix2[i][j] = matrix[j][i]
+    return matrix2
+
+
+h = histogram(x)
+c = convertDictToMatrix(h)
+# plotMatrix(c)
+print(c)
